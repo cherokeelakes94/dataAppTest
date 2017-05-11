18ca6edc30997e108a9f4f17160ab636978e2a5d
cherokeelakes94 <cherokeelakes94@gmail.com>
2017-05-10 21:42:57 -0400

    Update logged-in-state.js

--- js/logged-in-state.js
+++ js/logged-in-state.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
   firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     document.getElementById("login-div").style.display = 'none';
-	document.getElementById("div-status").classList.remove("disabled");
+	document.getElementById("div-my-rewards").classList.remove("disabled");
   } else {
     document.getElementById("login-div").style.display = 'block';
 	
-	document.getElementById("div-status").classList.add("disabled");
+	document.getElementById("div-my-rewards").classList.add("disabled");
 	document.getElementById("div-skills").classList.add("disabled");
-	document.getElementById("div-journal").classList.add('disabled');
+	document.getElementById("div-my-profile").classList.add('disabled');
 	document.getElementById("div-attributes").classList.add("disabled");	
   }
 });
